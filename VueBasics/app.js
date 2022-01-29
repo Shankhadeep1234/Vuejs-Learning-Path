c198a6f2691be737c17aae90de06dd2662dc5e67
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-01-29 18:52:13 +0530

    v-for for looping

--- VueBasics/app.js
+++ VueBasics/app.js
@@ -1,27 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "The Final Empire",
-      author: "Brandon Samsung",
-      age: 45,
       showBooks: true,
-      x: 0,
-      y: 0,
+      books: [
+        { title: "name of the wind", author: "patrick rothfuss" },
+        { title: "the way of kings", author: "brandon sanderson" },
+        { title: "the final empire", author: "brandon sanderson" },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
-    },
-    handleEvent(event, data) {
-      console.log(event.type);
-      if (data) {
-        console.log(data);
-      }
-    },
-    handleMousemove(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
     },
   },
 });
