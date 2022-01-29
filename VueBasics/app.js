81bc49d7f057ab8a957a38793046a7830934e20c
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-01-29 19:35:34 +0530

    dynamic class binding

--- VueBasics/app.js
+++ VueBasics/app.js
@@ -2,16 +2,35 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
+      url: "www.google.com",
       books: [
-        { title: "name of the wind", author: "patrick rothfuss" },
-        { title: "the way of kings", author: "brandon sanderson" },
-        { title: "the final empire", author: "brandon sanderson" },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
       ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
     },
   },
 });
