af43143480d08bb4c6fc51b431a724952944f3e7
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-01-29 18:37:34 +0530

    mouse events

--- VueBasics/app.js
+++ VueBasics/app.js
@@ -5,11 +5,23 @@ const app = Vue.createApp({
       author: "Brandon Samsung",
       age: 45,
       showBooks: true,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(event, data) {
+      console.log(event.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     },
   },
 });
